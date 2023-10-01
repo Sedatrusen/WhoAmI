11575ecba08dbbc3ae407742093c87e48a84c2dd
sedat ruşen <sedatrusen@hotmail.com>
2023-10-01 15:19:01 +0300

    adding repositories to Application layer

---
+++ WhoAmI.Application/Repositories/IMyUserRepositery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhoAmI.Core.Application;
+using WhoAmI.Domain.Entities;
+
+namespace WhoAmI.Application.Repositories
+{
+    internal interface IMyUserRepositery : IGenericRepository<MyUser,Guid>
+    {
+    }
+}
---
+++ WhoAmI.Core/Application/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhoAmI.Core.Domain;
+
+namespace WhoAmI.Core.Application
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<TEntity,TId> Repository<TEntity,TId>() where TEntity : BaseAuditableEntity<TId>;
+        Task<int> Save(CancellationToken cancellationToken);
+        Task<int> SaveAndRemoveCache(CancellationToken cancellationToken, params string[] cacheKeys);
+        Task Rollback();
+    }
+}
