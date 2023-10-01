feebd896411eef7d2475a578548d0b2c43121421
sedat ruşen <sedatrusen@hotmail.com>
2023-10-01 14:54:16 +0300

    adding usertype and quiz collection to myUser

--- WhoAmI.Domain/Entities/MyUser.cs
+++ WhoAmI.Domain/Entities/MyUser.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WhoAmI.Core.Domain;
+using WhoAmI.Domain.Enums;
 
 namespace WhoAmI.Domain.Entities
 {
-    public class AppUser : BaseAuditableEntity<Guid>
+    public class MyUser : BaseAuditableEntity<Guid>
     {
         public required string Name { get; set; }
         public required string Surname { get; set; }
         public required string Mail { get; set; }
         public required string Password { get; set; }
 
+        public required Collection<Quiz> Quizzes { get; set; }
+        public UserType UserType { get; set; }
       
     }
 }
