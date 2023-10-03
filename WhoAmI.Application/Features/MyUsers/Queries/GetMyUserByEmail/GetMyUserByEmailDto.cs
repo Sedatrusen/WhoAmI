255c94262977c7a6c5873a1bb2cbf9edbe84dfbd
sedat ruşen <sedatrusen@hotmail.com>
2023-10-03 19:03:55 +0300

    Adding my user dto

---
+++ WhoAmI.Application/Features/MyUsers/Queries/GetMyUserByEmail/GetMyUserByEmailDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhoAmI.Application.Common.Mappings;
+using WhoAmI.Domain.Entities;
+using WhoAmI.Domain.Enums;
+
+namespace WhoAmI.Application.Features.MyUsers.Queries.GetMyUserByEmail
+{
+    public class GetMyUserByEmailDto : IMapFrom<MyUser>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public Collection<Quiz>? Quizzes { get; set; }
+        public UserType UserType { get; set; }
+    }
+}
