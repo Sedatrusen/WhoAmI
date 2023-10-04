1e84d633f6e1370b2e886633b0ba4f264323a61d
sedat ruşen <sedatrusen@hotmail.com>
2023-10-04 18:40:45 +0300

    ^^

---
+++ WhoAmI.Persistence/Repositories/QuizResultRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhoAmI.Application.Repositories;
+using WhoAmI.Core.Application;
+using WhoAmI.Domain.Entities;
+
+namespace WhoAmI.Persistence.Repositories
+{
+    public class QuizResultRepository : IQuizResultRepository
+    {
+        private readonly IGenericRepository<QuizResult> _repository;
+
+        public QuizResultRepository(IGenericRepository<QuizResult> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<QuizResult>> GetAllQuizResultsByQuizIdAsync(int id)
+        {
+            return await _repository.Entities.Where(x => x.QuizId == id).ToListAsync();
+        }
+    }
+}
